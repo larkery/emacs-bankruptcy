f9f63644c3365734fce01171207de9605d142a16
Tom Hinton <tom.hinton@cse.org.uk>
2015-09-29 17:05:11 +0100

    turn on which-function-mode

--- packages.el
+++ packages.el
@@ -583,3 +583,5 @@ Search: _a_g      |  _g_tags upd   |  find _T_ag   |  _o_ccur    |  _G_rep
 
 (defmath as (e units)
   (math-convert-units e units))
+
+(add-hook 'prog-mode-hook #'which-function-mode)
