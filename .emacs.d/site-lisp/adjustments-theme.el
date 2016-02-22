d76eb18f51cff3a43814003afdc7520766271dd8
Tom Hinton <tom.hinton@cse.org.uk>
2016-02-22 14:16:13 +0000

    minor theme tweaks

--- .emacs.d/site-lisp/adjustments-theme.el
+++ .emacs.d/site-lisp/adjustments-theme.el
@@ -3,16 +3,16 @@
 
 (custom-theme-set-faces
  'adjustments
- '(default ((t :height 100 :foreground "#c7c7c7")))
+ '(default ((t :height 100)))
  '(ido-first-match ((t (:inverse-video t))))
  '(ido-only-match ((t (:inverse-video t))))
  '(cursor ((t (:background "#ffffff"))))
- '(outline-1 ((t (:height 1.5 :foreground "#ffffff"))))
- '(outline-2 ((t (:height 1.4 :foreground "#ffffff"))))
- '(outline-3 ((t (:height 1.3 :foreground "#ffffff"))))
- '(outline-4 ((t (:height 1.2 :foreground "#ffffff"))))
- '(outline-5 ((t (:height 1.1 :foreground "#ffffff"))))
- '(message-cited-text ((t (:inherit shadow))))
+ '(outline-1 ((t (:height 1.4 :underline "#555555" :foreground "#ffffff"))))
+ '(outline-2 ((t (:height 1.3 :underline "#555555" :foreground "#ffffff"))))
+ '(outline-3 ((t (:height 1.2 :underline "#555555" :foreground "#ffffff"))))
+ '(outline-4 ((t (:height 1.1 :underline "#555555" :foreground "#ffffff"))))
+ '(outline-5 ((t (:height 1.0 :underline "#555555" :foreground "#ffffff"))))
+ '(message-cited-text ((t (:inherit shadow :slant italic))))
  )
 
 (provide-theme 'adjustments)
