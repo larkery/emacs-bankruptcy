38da5ede7ab11c2c1cf2098f56e1de36aeea7c75
Tom Hinton <tom.hinton@cse.org.uk>
2016-06-24 17:35:50 +0100

    insert path

--- packages.d/c-keybindings.el
+++ packages.d/c-keybindings.el
@@ -70,6 +70,28 @@
 
       (find-alternate-file (concat "/sudo:root@" (system-name) ":" the-place)))))
 
+
+(defun my-insert-file-name (filename &optional args)
+  "Insert name of file FILENAME into buffer after point.
+
+  Prefixed with \\[universal-argument], expand the file name to
+  its fully canocalized path.  See `expand-file-name'.
+
+  Prefixed with \\[negative-argument], use relative path to file
+  name from current directory, `default-directory'.  See
+  `file-relative-name'.
+
+  The default with no prefix is to insert the file name exactly as
+  it appears in the minibuffer prompt."
+  ;; Based on insert-file in Emacs -- ashawley 20080926
+  (interactive "*fInsert file name: \nP")
+  (cond ((eq '- args)
+         (insert (expand-file-name filename)))
+        ((not (null args))
+         (insert filename))
+        (t
+         (insert (file-relative-name filename)))))
+
 ;;(unbind-key "C-v")
 
 (dolist (binding
@@ -83,6 +105,8 @@
        ("C-c t" . my-tabulate)
        ("<f1>" . delete-other-windows)
        ("<f2>" . my-split-window)
+       ("C-c C-/" . my-insert-file-name)
+
   ;;     ("C-v f" . delete-other-windows)
     ;;   ("C-v d" . delete-window)
 ;;       ("C-v c" . ctl-x-5-prefix)
