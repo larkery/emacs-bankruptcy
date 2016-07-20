05d541760665ad90f05e01311846d70d27d4eac2
Tom Hinton <t@larkery.com>
2016-07-20 21:49:14 +0100

    toggly theme

--- packages.d/e-theme.el
+++ packages.d/e-theme.el
@@ -1,15 +1,20 @@
-(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes"))
-(load-theme 'tango t)
-(load-theme 'tweaks t)
+(req-package tangotango-theme
+  :config
+  (defvar dark-theme 'tangotango)
+  (defvar light-theme 'tango)
+  (defvar dark-mode nil)
 
-(defvar dark-mode t)
+  (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes"))
 
-(defun toggle-dark-mode ()
-  (interactive)
-  (dolist (theme custom-enabled-themes) (disable-theme theme))
+  (defun toggle-dark-mode ()
+    (interactive)
+    (dolist (theme custom-enabled-themes) (disable-theme theme))
 
-  (setq dark-mode (not dark-mode))
+    (setq dark-mode (not dark-mode))
 
-  (load-theme (if dark-mode 'tango-dark 'tango) t)
-  (load-theme 'tweaks t)
-  (shell-command (if dark-mode "xrdb ~/.Xresources -DDARK" "xrdb ~/.Xresources -UDARK")) )
+    (load-theme (if dark-mode dark-theme light-theme) t)
+    (load-theme 'tweaks t)
+    (shell-command (if dark-mode "xrdb ~/.Xresources -DDARK" "xrdb ~/.Xresources -UDARK")) )
+
+  (bind-key "<f11>" 'toggle-dark-mode)
+  (toggle-dark-mode))
