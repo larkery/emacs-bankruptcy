570f4707d937da77ab08baf48631035497f28e61
Tom Hinton <tom.hinton@cse.org.uk>
2016-08-04 17:07:31 +0100

    small edit

--- themes/tweaks-theme.el
+++ themes/tweaks-theme.el
@@ -5,7 +5,11 @@
 
  '(ivy-remote ((t (:inherit error))))
 
- '(show-paren-match ((t (:background nil :foreground "cyan" :weight bold :underline t))))
+ '(show-paren-match ((t (:background nil :foreground nil :weight bold
+;;                                     :box (:line-width -1)
+
+                                     :underline t
+                                     ))))
  '(show-paren-mismatch ((t (:background "black" :foreground "red" :weight bold))))
 
  '(org-todo ((t (:foreground "white" :background "red3"))))
@@ -20,25 +24,25 @@
 
  '(w3m-lnum ((t (:background "#F0F0F0" :foreground "dark orange"))))
 
- '(outline-1 ((t (:height 1.3))))
- '(outline-2 ((t (:height 1.3))))
- '(outline-3 ((t (:height 1.3))))
- '(outline-4 ((t (:height 1.3))))
- '(outline-5 ((t (:height 1.3))))
- '(outline-6 ((t (:height 1.3))))
- '(outline-7 ((t (:height 1.3))))
- '(outline-8 ((t (:height 1.3))))
- '(outline-9 ((t (:height 1.3))))
+ '(outline-1 ((t (:height 1.5 :box "grey50"))))
+ '(outline-2 ((t (:height 1.2 :box "grey50"))))
+ '(outline-3 ((t (:height 1.1 :box "grey50"))))
+ '(outline-4 ((t (:height 1.1 :box "grey50"))))
+ '(outline-5 ((t (:height 1.1 :box "grey50"))))
+ '(outline-6 ((t (:height 1.1 :box "grey50"))))
+ '(outline-7 ((t (:height 1.1 :box "grey50"))))
+ '(outline-8 ((t (:height 1.1 :box "grey50"))))
+ '(outline-9 ((t (:height 1.1 :box "grey50"))))
 
- '(org-level-1 ((t (:height 1.3))))
- '(org-level-2 ((t (:height 1.3))))
- '(org-level-3 ((t (:height 1.3))))
- '(org-level-4 ((t (:height 1.3))))
- '(org-level-5 ((t (:height 1.3))))
- '(org-level-6 ((t (:height 1.3))))
- '(org-level-7 ((t (:height 1.3))))
- '(org-level-8 ((t (:height 1.3))))
- '(org-level-9 ((t (:height 1.3))))
+ '(org-level-1 ((t (:height 1.5 :box "grey50"))))
+ '(org-level-2 ((t (:height 1.2 :box "grey50"))))
+ '(org-level-3 ((t (:height 1.1 :box "grey50"))))
+ '(org-level-4 ((t (:height 1.1 :box "grey50"))))
+ '(org-level-5 ((t (:height 1.1 :box "grey50"))))
+ '(org-level-6 ((t (:height 1.1 :box "grey50"))))
+ '(org-level-7 ((t (:height 1.1 :box "grey50"))))
+ '(org-level-8 ((t (:height 1.1 :box "grey50"))))
+ '(org-level-9 ((t (:height 1.1 :box "grey50"))))
 
  '(message-cited-text ((t (:weight bold))))
 
