fb77311cc837d3519976cfef6f8c63855a503ba7
Tom Hinton <tom.hinton@cse.org.uk>
2017-02-02 18:25:24 +0000

    fix alignment

--- packages.d/f-maths.el
+++ packages.d/f-maths.el
@@ -1,24 +1,21 @@
 (defun math-eval-line ()
     (interactive)
 
-    (save-excursion
-      (save-restriction
-        (let ((bounds (bounds-of-thing-at-point 'line)))
-          (narrow-to-region (car bounds) (cdr bounds))
-          (goto-char (point-min))
+    (save-match-data
+      (save-excursion
+       (save-restriction
+         (let ((bounds (bounds-of-thing-at-point 'line))
+               equal-sign)
+           (narrow-to-region (car bounds) (cdr bounds))
+           (goto-char (point-min))
 
-          (let* ((eqlsign (search-forward "=" nil t))
-                 (result (condition-case nil
-                             (calc-eval
-                              (buffer-substring-no-properties
-                               (point-min)
-                               (- (or eqlsign (1+ (point-max))) 1)))
-                           (error "error"))))
-            (when eqlsign
-              (delete-region (- eqlsign 1) (point-max)))
-            (goto-char (point-max))
-            (unless (looking-at " ") (insert " "))
-            (insert (format "= %s" result)))
-          ))))
+           (when (setq equal-sign (search-forward-regexp "\\([[:space:]]*=\\)\\|\\([[:space:]]+$\\)" nil t))
+             (delete-region (match-beginning 0) (point-max)))
+
+           (goto-char (point-max))
+           (insert (format " = %s" (calc-eval
+                                    (buffer-substring-no-properties
+                                     (point-min) (point-max))))))
+         ))))
 
 (bind-key "<C-return>" #'math-eval-line)
