d3ba27a596b5798effd0f67409d5b3ea1f745af4
Tom Hinton <tom.hinton@cse.org.uk>
2017-03-22 14:54:11 +0000

    stop using date tree as it is a nuisance

---
+++ site-lisp/org-log.el
@@ -0,0 +1,29 @@
+(defcustom org-log-location
+  '("~/notes/journal/%Y.org"
+    "* %B"
+    "** %A %e")
+  "A list of strings, each of which will be used with `format-time-string'.
+The first will find a file, and the rest headings and subheadings and so on.")
+
+(defun org-log-goto (&optional date)
+  (interactive)
+
+  (cl-loop for part in org-log-location
+           for ix from 0
+           do (setq part (format-time-string part date))
+           do (if (zerop ix)
+                  (progn (find-file part)
+                         (goto-char (point-min)))
+                (unless (search-forward-regexp (rx-to-string `(seq bol ,part)) nil t)
+                  (insert part "\n"))
+                (org-narrow-to-element)))
+  (goto-char (point-max))
+  (widen)
+  (org-cycle '(4))
+  (org-reveal))
+
+
+
+
+
+(provide 'org-log)
