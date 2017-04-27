8abec36f7ac29a79bd49fea41f74a835d98e2f3d
Tom Hinton <t@larkery.com>
2017-04-27 18:27:12 +0100

    other god keys

--- packages.d/f-god-mode.el
+++ packages.d/f-god-mode.el
@@ -1,3 +1,5 @@
+(initsplit-this-file bos "god-")
+
 (req-package god-mode
   :commands god-local-mode god-mode-isearch-activate
   :bind ("<escape>" . god-local-mode)
@@ -6,7 +8,7 @@
   :config
   (require 'god-mode-isearch)
   (define-key god-mode-isearch-map (kbd "<escape>") 'god-mode-isearch-disable)
-  (define-key god-local-mode-map (kbd ".") 'repeat)
+  (define-key god-local-mode-map (kbd "g") 'repeat)
 
   (add-to-list 'god-exempt-major-modes 'notmuch-search-mode)
   (add-to-list 'god-exempt-major-modes 'notmuch-show-mode)
@@ -17,22 +19,26 @@
   (defun god-cursor ()
     (if god-local-mode
         (progn
-          ;; (push (face-remap-add-relative 'hl-line :background "darkcyan")
-          ;;       god-cursor-face-remapping)
-
-          (push (face-remap-add-relative 'mode-line :background "OrangeRed4" :foreground "white")
+          (push (face-remap-add-relative
+                 'mode-line
+                 :background "orangered4" :foreground "white")
                 god-cursor-face-remapping)
-          (setq cursor-type 'bar)
-          ;; (hl-line-mode 1)
-          )
+          (setq cursor-type 'bar))
 
       (progn (dolist (mapping god-cursor-face-remapping)
                (face-remap-remove-relative mapping))
              (setq god-cursor-face-remapping nil)
-             ;; (hl-line-mode -1)
-
              (setq cursor-type t))))
-
 
   (add-hook 'god-mode-enabled-hook #'god-cursor)
   (add-hook 'god-mode-disabled-hook #'god-cursor))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(god-mod-alist
+   (quote
+    ((nil . "C-")
+     ("." . "M-")
+     ("," . "C-M-")))))
