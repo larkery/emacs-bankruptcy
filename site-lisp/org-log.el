fc315638fe8601016015dbfb7e8c8fa693caf9ff
Tom Hinton <tom.hinton@cse.org.uk>
2017-05-16 17:53:37 +0100

    jump to day in log from calendar

--- site-lisp/org-log.el
+++ site-lisp/org-log.el
@@ -8,20 +8,29 @@ The first will find a file, and the rest headings and subheadings and so on.")
 (defun org-log-goto (&optional date)
   (interactive)
 
-  (cl-loop for part in org-log-location
-           for ix from 0
-           do (setq part (format-time-string part date))
-           do (if (zerop ix)
-                  (progn (find-file part)
-                         (goto-char (point-min)))
-                (unless (search-forward-regexp (rx-to-string `(seq bol ,part)) nil t)
-                  (goto-char (point-max))
-                  (insert part "\n"))
-                (org-narrow-to-element)))
-  (goto-char (point-max))
-  (widen)
-  (org-cycle '(4))
-  (org-show-context 'ancestors))
+  (let ((ff-command #'find-file))
+
+    (when (eq major-mode 'calendar-mode)
+      (let ((dt (calendar-cursor-to-date)))
+        (when dt
+          (setq date (encode-time 0 0 0 (nth 1 dt) (nth 0 dt) (nth 2 dt))
+                ff-command #'find-file-other-window))))
+
+    (cl-loop for part in org-log-location
+             for ix from 0
+             do (setq part (format-time-string part date))
+             do (if (zerop ix)
+                    (progn (funcall ff-command part)
+                           (goto-char (point-min)))
+                  (unless (search-forward-regexp (rx-to-string `(seq bol ,part)) nil t)
+                    (goto-char (point-max))
+                    (insert part "\n"))
+                  (org-narrow-to-element)))
+    (goto-char (point-max))
+    (widen)
+    (org-cycle '(4))
+    (org-show-context 'ancestors)))
+
 
 
 (provide 'org-log)
