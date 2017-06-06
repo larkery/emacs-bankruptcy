9a69af72e3c4b3b38299114f01a787f22698e728
Tom Hinton <t@larkery.com>
2017-06-06 21:08:16 +0100

    virtual buffers in ivy have full paths

--- packages.d/e-ivy.el
+++ packages.d/e-ivy.el
@@ -32,7 +32,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(counsel-find-file-ignore-regexp "\\`\\.")
- '(ivy-use-virtual-buffers t))
+ '(ivy-use-virtual-buffers t)
+ '(ivy-virtual-abbreviate (quote full)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
