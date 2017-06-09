11fa090a2c6d4ee9366e65632c9aa84664ab35e4
Tom Hinton <tom.hinton@cse.org.uk>
2017-06-09 10:20:49 +0100

    truncate paths and fold in host

--- site-lisp/ivy-fancy-buffer-menu.el
+++ site-lisp/ivy-fancy-buffer-menu.el
@@ -1,28 +1,57 @@
 
 (defun add-face (str prop)
   (if prop
-      (propertize str 'face prop)
+      (propertize str 'face
+                  (append (get-text-property 0 'face str) prop))
     str))
-
 
 (defun ivy--fancy-buffer-mode (buffer-name)
   (if-let ((buffer (get-buffer buffer-name)))
       (with-current-buffer buffer
-        (add-face
-         mode-name
-         (if buffer-file-name 'mode-line-buffer-id 'mode-line-emphasis)))
-    (add-face "virt" 'shadow)))
+        mode-name)
+    ""))
 
 (defun ivy--fancy-buffer-project (buffer-name)
   (if-let ((buffer (get-buffer buffer-name)))
       (with-current-buffer buffer (projectile-project-name))
     ""))
 
+(defun ivy--fancy-compress-path (path mlen)
+  (let ((len (length path)))
+    (when (> len mlen)
+
+      (let ((parts (split-string (substring path 0 (- (length path) 1)) "/")))
+        (setq path
+              (loop for part in parts
+                    unless (zerop (length part))
+
+                    if (and (> len mlen)
+                            (> (length part) 1))
+                    concat (substring part 0 1) and
+                    concat "…" and
+                    do (setq len (- len (- (length part) 2)))
+                    else concat part
+
+                    concat "/")))))
+  path)
+
 (defun ivy--fancy-buffer-directory (buffer-name)
-  (abbreviate-file-name
-   (if-let ((buffer (get-buffer buffer-name)))
-       (with-current-buffer buffer default-directory)
-     (file-name-directory (cdr (assq buffer-name ivy--virtual-buffers))))))
+  (let ((full-name (abbreviate-file-name
+                    (if-let ((buffer (get-buffer buffer-name)))
+                        (with-current-buffer buffer default-directory)
+                      (file-name-directory (cdr (assq buffer-name ivy--virtual-buffers)))))))
+    (if (file-remote-p full-name)
+        (let ((parts (tramp-dissect-file-name full-name)))
+          (concat
+           (add-face
+            (concat
+             (tramp-file-name-user parts)
+             (when (> (length (tramp-file-name-user parts)) 0) "@")
+             (car (split-string (tramp-file-name-host parts) "\\.")))
+            '(:background "darkred"))
+           ":"
+           (ivy--fancy-compress-path (tramp-file-name-localname parts) 40)))
+      (ivy--fancy-compress-path full-name 50))))
 
 (defun ivy--fancy-buffer-host (buffer-name)
   (or (let* ((file-name
@@ -37,8 +66,8 @@
 
 (defvar ivy--fancy-buffer-columns
   `((identity "%-40.45s")
-    (ivy--fancy-buffer-host " %8.8s  " (:foreground "red"))
-    (ivy--fancy-buffer-mode " %-9.9s  ")
+
+    (ivy--fancy-buffer-mode " %9.9s  " (:foreground "darkorange"))
     (ivy--fancy-buffer-project " %-8.8s  " (:foreground "darkcyan"))
     (ivy--fancy-buffer-directory " %s")))
 
