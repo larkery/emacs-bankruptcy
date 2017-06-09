ecea0b0ec592d428e2df1c8266e8958fbbfb99cd
Tom Hinton <tom.hinton@cse.org.uk>
2017-06-09 13:02:56 +0100

    remove old  hacks

--- packages.d/a-hack-tramp.el
+++ packages.d/a-hack-tramp.el
@@ -1,17 +1,21 @@
-(defvar tramp-add-space-to-command nil)
+;; (defvar tramp-add-space-to-command nil)
 
-(defun prefix-tramp-with-space (o &rest args)
-  "The old bug is fixed, but we want to add a space sometimes."
 
-  (let ((tramp-add-space-to-command t))
-    (apply o args)))
+;; (defun prefix-tramp-with-space (o &rest args)
+;;   "The old bug is fixed, but we want to add a space sometimes."
 
-(defun tramp-send-command-space (o vec command &optional neveropen nooutput)
-  (funcall o vec
-           (if tramp-add-space-to-command
-               (concat " " command)
-             command)
-           neveropen nooutput))
+;;   (let ((tramp-add-space-to-command t))
+;;     (apply o args)))
 
-(advice-add 'tramp-open-shell :around #'prefix-tramp-with-space)
-(advice-add 'tramp-send-command :around #'tramp-send-command-space)
+
+;; (defun tramp-send-command-space (o vec command &optional neveropen nooutput)
+;;   (funcall o vec
+;;            (if tramp-add-space-to-command
+;;                (concat " " command)
+;;              command)
+;;            neveropen nooutput))
+
+
+;; (advice-add 'tramp-open-shell :around #'prefix-tramp-with-space)
+
+;; (advice-add 'tramp-send-command :around #'tramp-send-command-space)
