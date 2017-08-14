7c6f577a326b37e6dfc6d4854fc0648f7748813d
Tom Hinton <tom.hinton@cse.org.uk>
2017-08-14 18:35:04 +0100

    time of day fix

--- packages.d/f-mail.el
+++ packages.d/f-mail.el
@@ -158,7 +158,7 @@
            (minute (nth 1 now))
            (dow (nth 6 now)))
       (or (= 0 dow) (= 6 dow)
-          (< hour 9) (and (> hour 17) (> minute 30)))))
+          (< hour 9) (and (> (+ hour (/ minute 60.0)) 17.5)))))
 
   (defun guess-address-advice (o &rest args)
     (if (outside-working-hours)
