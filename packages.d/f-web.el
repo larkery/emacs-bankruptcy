569e7c61c327aa1d000836992fc10ce56034ad1b
Tom Hinton <t@larkery.com>
2017-12-06 12:28:49 +0000

    less stupid indent

--- packages.d/f-web.el
+++ packages.d/f-web.el
@@ -7,3 +7,9 @@
 (req-package js2-mode
   :mode (("\\.js\\'" . js2-mode))
   )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(web-mode-markup-indent-offset 2))
