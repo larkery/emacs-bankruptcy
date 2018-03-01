2a4993e233add53964be254e789440689c8e9f4d
Tom Hinton <t@larkery.com>
2018-03-01 17:55:51 +0000

    use someone else's scratch thing

--- packages.d/f-persist-scratch.el
+++ packages.d/f-persist-scratch.el
@@ -1,16 +1,23 @@
 ;; -*- lexical-binding: t -*-
-(defvar my-scratch-state (concat (my-state-dir "") "scratch.el"))
+
+(use-package persistent-scratch
+  :config
+  (persistent-scratch-setup-default))
+;; (defvar my-scratch-state (concat (my-state-dir "") "scratch.el"))
+
 
 ;; on startup, recover scratch
 
-(when (file-exists-p my-scratch-state)
-  (with-current-buffer (get-buffer-create "*scratch*")
-    (insert-file-contents my-scratch-state nil nil nil t)))
+;; (when (file-exists-p my-scratch-state)
+;;   (with-current-buffer (get-buffer-create "*scratch*")
+;;     (insert-file-contents my-scratch-state nil nil nil t)))
 
-(defun my-save-scratch ()
-  (let ((scratch-buffer (get-buffer "*scratch*")))
-    (when scratch-buffer
-      (with-current-buffer scratch-buffer
-        (write-file my-scratch-state)))))
 
-(add-hook 'kill-emacs-hook #'my-save-scratch)
+;; (defun my-save-scratch ()
+;;   (let ((scratch-buffer (get-buffer "*scratch*")))
+;;     (when scratch-buffer
+;;       (with-current-buffer scratch-buffer
+;;         (write-file my-scratch-state)))))
+
+
+;; (add-hook 'kill-emacs-hook #'my-save-scratch)
